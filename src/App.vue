2bbf60c363cde89ddd2b8ff61411fdf4933ce222
AlexxMart <alex@ravn.co>
2021-04-10 14:17:31 -0600

    :lipstick: fixed mobile styles

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav-bar />
-    
+
     <div class="main-wrapper">
       <div id="about">
         <h2>Bio</h2>
@@ -12,7 +12,7 @@
         />
         <div class="bio">
           <p>
-            My name is Alexx Martínez and I am a Web Developer from El Salvador,
+            My name is Alexx Martínez and I am a Web and Mobile Developer from El Salvador,
             Central America.
           </p>
 
@@ -33,7 +33,7 @@
           </p>
 
           <p>
-            Currently I have a full-time web developer job at
+            Currently I have a full-time Web and React Native developer job at
             <a
               href="https://www.ravn.co/"
               target="_blank"
@@ -45,8 +45,8 @@
               href="https://www.poderosaisherpower.org/"
               target="_blank"
               rel="noreferrer noopener"
-              >Poderosa Is Her Power.</a
-            >
+              >Poderosa Is Her Power</a
+            >. Where we work to battle gender-based violence.
           </p>
         </div>
       </div>
@@ -55,60 +55,64 @@
       <projects-grid />
 
       <footer id="contactMe">
-      <h2>Contact Me</h2>
-      <div class="contact-grid">
-        <a
-          href="https://github.com/AlexxMart"
-          target="_blank"
-          rel="noreferrer noopener"
-          ><font-awesome-icon :icon="['fab', 'github']" /></a>
+        <h2>Contact Me</h2>
+        <div class="contact-grid">
+          <a
+            href="https://github.com/AlexxMart"
+            target="_blank"
+            rel="noreferrer noopener"
+            ><font-awesome-icon :icon="['fab', 'github']"
+          /></a>
 
-        <a
-          href="https://www.freecodecamp.org/alexxmart"
-          target="_blank"
-          rel="noreferrer noopener"
-          ><font-awesome-icon :icon="['fab', 'free-code-camp']" /></a>
+          <a
+            href="https://www.freecodecamp.org/alexxmart"
+            target="_blank"
+            rel="noreferrer noopener"
+            ><font-awesome-icon :icon="['fab', 'free-code-camp']"
+          /></a>
 
-        <a
-          href="https://www.linkedin.com/in/oscar-alexander-mata-5227a213a/"
-          target="_blank"
-          rel="noreferrer noopener"
-          ><font-awesome-icon :icon="['fab', 'linkedin']" /></a>
+          <a
+            href="https://www.linkedin.com/in/oscar-alexander-mata-5227a213a/"
+            target="_blank"
+            rel="noreferrer noopener"
+            ><font-awesome-icon :icon="['fab', 'linkedin']"
+          /></a>
 
-        <a
-          href="https://codepen.io/Alexxander/"
-          target="_blank"
-          rel="noreferrer noopener"
-          ><font-awesome-icon :icon="['fab', 'codepen']" /></a>
+          <a
+            href="https://codepen.io/Alexxander/"
+            target="_blank"
+            rel="noreferrer noopener"
+            ><font-awesome-icon :icon="['fab', 'codepen']"
+          /></a>
 
-        <a
-          href="https://www.instagram.com/devalexxander/"
-          target="_blank"
-          rel="noreferrer noopener"
-          ><font-awesome-icon :icon="['fab', 'instagram']" /></a>
+          <a
+            href="https://www.instagram.com/devalexxander/"
+            target="_blank"
+            rel="noreferrer noopener"
+            ><font-awesome-icon :icon="['fab', 'instagram']"
+          /></a>
 
-        <a
-          href="https://twitter.com/Alexx__Mata"
-          target="_blank"
-          rel="noreferrer noopener"
-          ><font-awesome-icon :icon="['fab', 'twitter']" /></a>
+          <a
+            href="https://twitter.com/Alexx__Mata"
+            target="_blank"
+            rel="noreferrer noopener"
+            ><font-awesome-icon :icon="['fab', 'twitter']"
+          /></a>
 
-        <a class="email" href="mailto:devalexander19@gmail.com"
-          ><font-awesome-icon :icon="['fa', 'envelope']" />
-          <span>devalexander19@gmail.com</span></a
-        >
-      </div>
-    </footer>
-
+          <a class="email" href="mailto:devalexander19@gmail.com"
+            ><font-awesome-icon :icon="['fa', 'envelope']" />
+            <span>devalexander19@gmail.com</span></a
+          >
+        </div>
+      </footer>
     </div>
   </div>
 </template>
 
 <script>
-
-import NavBar from "./components/NavBar.vue";;
-import Stack from './components/Stack.vue';
-import ProjectsGrid from './components/ProjectsGrid.vue';
+import NavBar from "./components/NavBar.vue";
+import Stack from "./components/Stack.vue";
+import ProjectsGrid from "./components/ProjectsGrid.vue";
 
 export default {
   components: {
@@ -116,9 +120,9 @@ export default {
     Stack,
     ProjectsGrid,
   },
-  beforeCreate () {
-    document.title = 'Alexx\'s Portfolio';
-  }
+  beforeCreate() {
+    document.title = "Alexx's Portfolio";
+  },
 };
 </script>
 
@@ -133,92 +137,99 @@ body {
 }
 
 body {
-  background: #ff416c; /* fallback for old browsers */
+  background: #ff416c;
   background: -webkit-linear-gradient(
     to top,
     #ff4b2b,
     #ff416c
-  ); /* Chrome 10-25, Safari 5.1-6 */
+  );
   background: linear-gradient(
     to top,
     #ff4b2b,
     #ff416c
-  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  );
   padding-top: 100px;
 }
 
 .main-wrapper {
-    width: 80%;
-    background-color: #EFF2FE;
-    border-radius: 20px;
-    padding: 40px 0;
-    margin: 0 auto;
+  width: 80%;
+  background-color: #eff2fe;
+  border-radius: 20px;
+  padding: 40px 0;
+  margin: 0 auto;
 }
 
-#about > h2, #stack > h2, #projects > h2, #contactMe > h2 {
-    text-align: center;
-    color: #232323;
-    font-family: 'Open Sans';
-    margin-bottom: 40px;
+#about > h2,
+#stack > h2,
+#projects > h2,
+#contactMe > h2 {
+  text-align: center;
+  color: #232323;
+  font-family: "Open Sans";
+  margin-bottom: 40px;
 }
 
 #profile-picture {
-    display: block;
-    width: 200px;
-    border-radius: 50%;
-    margin: 0 auto -100px auto;
+  display: block;
+  width: 200px;
+  border-radius: 50%;
+  margin: 0 auto -100px auto;
 }
 
 .bio {
-    width: 50%;
-    border-radius: 20px;
-    border-top: 5px solid #FF4B2B;
-    box-shadow: 0 10px 30px #e0dfdf;
-    padding: 100px 40px 40px 40px;
-    margin: 0 auto;
-    transition: 0.3s;
+  width: 50%;
+  border-radius: 20px;
+  border-top: 5px solid #ff4b2b;
+  box-shadow: 0 10px 30px #e0dfdf;
+  padding: 100px 40px 40px 40px;
+  margin: 0 auto;
+  transition: 0.3s;
 }
 
 .bio:hover {
-    box-shadow: 0 10px 10px #c9c8c8;
-    transition: 0.3s;
+  box-shadow: 0 10px 10px #c9c8c8;
+  transition: 0.3s;
 }
 
 .bio > p {
-    font-family: 'Roboto Mono';
-    font-size: 1.1em;
-    color: #232323;
-    line-height: 1.7em;
-    margin: 25px 0;
+  font-family: "Roboto Mono";
+  font-size: 1.1em;
+  color: #232323;
+  line-height: 1.7em;
+  margin: 25px 0;
 }
 
 .contact-grid {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    margin-bottom: 30px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-bottom: 30px;
 }
 
 .contact-grid > a > svg {
-    font-size: 24px;
-    color: #252525;
-    margin: 10px 20px;
-    transition: 0.3s;
+  font-size: 24px;
+  color: #252525;
+  margin: 10px 20px;
+  transition: 0.3s;
 }
 
 .contact-grid > a > svg:hover {
-    color: #FF4B2B;
-    transition: 0.3s;
+  color: #ff4b2b;
+  transition: 0.3s;
 }
 
 .email {
-    display: block;
-    width: 100%;
-    height: 30px;
-    line-height: 35px;
-    text-align: center;
-    color: #252525;
-    transition: 0.3s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 30px;
+  font-family: "Open Sans";
+  text-decoration: none;
+  line-height: 35px;
+  text-align: center;
+  color: #252525;
+  transition: 0.3s;
 }
 
 .email > svg {
@@ -227,57 +238,73 @@ body {
 }
 
 .email:hover {
-    color: #FF4B2B;
-    transition: 0.3s;
+  color: #ff4b2b;
+  transition: 0.3s;
 }
 
 .email > span {
-    vertical-align: middle;
-    padding-left: 15px;
+  vertical-align: middle;
+  padding-left: 15px;
 }
 
 @media all and (max-width: 801px) {
-    .main-wrapper {
-        width: 90%;
-    }
+  .main-wrapper {
+    width: 90%;
+  }
 
-    .bio {
-        width: 70%;
-    }
+  #navBar > ul {
+    width: 90%;
+    padding: 0;
+  }
+
+  #navBar > ul > li > a {
+    font-size: 16px;
+    padding: 5px !important;
+  }
+
+  .bio {
+    width: 70%;
+  }
 }
 
 @media all and (max-width: 415px) {
-    body {
-        padding-top: 70px;
-    }
+  body {
+    padding-top: 70px;
+  }
 
-    #navBar > ul {
-        width: 100%;
-    }
+  #navBar > ul {
+    width: 100%;
+    padding: 0;
+  }
 
-    #navBar > ul > li > a {
-        font-size: 14px;
-        padding: 5px !important;
-    }
+  #navBar > ul > li > a {
+    font-size: 14px;
+    padding: 5px !important;
+  }
 
-    .main-wrapper {
-        width: 95%;
-    }
+  .main-wrapper {
+    width: 95%;
+  }
 
-    .bio {
-        width: 90%;
-    }
+  .bio {
+    width: 70vw;
+  }
 
-    .stack-grid {
-        padding: 20px;
-    }
+  .stack-grid {
+    padding: 20px;
+  }
 
-    .stack-grid > .tech-logo {
-        max-height: 70px;
-    }
+  .stack-grid > .tech-logo {
+    max-height: 70px;
+    margin: 24px 40px;
+  }
 
-    .projects-grid > .projects-card {
-        max-width: 310px;
-    }
+  .projects-grid > .projects-card {
+    max-width: 310px;
+  }
+
+  .contact-grid > a > svg {
+    margin: 8px 16px;
+  }
 }
 </style>
