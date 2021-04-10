ca3b509bae38379b3f4bc3ec8077202daf620434
AlexxMart <alex@ravn.co>
2021-04-10 12:53:53 -0600

    :bug: fixed broken styles for navBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,25 +1,23 @@
 <template>
-	<div>
-		<div id="progressBar"></div>
-		<div id="progressBarContainer"></div>
-		<nav id="navBar">
-			<ul>
-				<li><a href="#about">About Me</a></li>
-				<li><a href="#stack">My Stack</a></li>
-				<li><a href="#projects">Projects</a></li>
-				<li><a href="#contactMe">Contact</a></li>
-			</ul>
-		</nav>
-	</div>
+  <div>
+    <div id="progressBar"></div>
+    <div id="progressBarContainer"></div>
+    <nav id="navBar">
+      <ul>
+        <li><a href="#about">About Me</a></li>
+        <li><a href="#stack">My Stack</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contactMe">Contact</a></li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 #progressBar {
   position: fixed;
   top: 0;
@@ -31,38 +29,50 @@ export default {
 }
 
 #navBar {
-    top: 0;
-    position: fixed;
-    background-color: #242424;
-    z-index: 10;
+  top: 0;
+  position: fixed;
+  background-color: #242424;
+  z-index: 10;
+  width: 100%;
 }
 
 #navBar > ul {
-    display: flex;
-    justify-content: space-around;
-    width: 80%;
-    margin: 0 auto;
+  display: flex;
+	justify-content: space-around;
+  width: 80%;
+  margin: 0 auto;
 }
 
 #navBar > ul > li {
-    text-align: center;
-    width: 100%;
-    transition: 0.3s;
+  list-style: none;
+  width: 100%;
+  transition: 0.3s;
 }
 
 #navBar > ul > li:hover {
-    background-color: #EFF2FE;
-    transition: 0.3s;
+  background-color: #eff2fe;
+  transition: 0.3s;
+
+  a {
+    color: #242424;
+  }
 }
 
 #navBar > ul > li > a {
-    font-family: 'Roboto Mono';
-    font-size: 20px;
-    transition: 0.3s;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 64px;
+  width: 100%;
+  text-decoration: none;
+  color: #eff2fe;
+  font-family: "Roboto Mono";
+  font-size: 20px;
+  transition: 0.3s;
 }
 
 #navBar > ul > li > a:hover {
-    color: #242424;
-    transition: 0.3s;
+  color: #242424;
+  transition: 0.3s;
 }
 </style>
